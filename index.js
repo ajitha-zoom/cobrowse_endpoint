c41f8114794e5d0552f1c6a42d95b2b80832fc43
ajitha-zoom <123167421+ajitha-zoom@users.noreply.github.com>
2025-03-14 18:30:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import { KJUR } from 'jsrsasign'
-import { inNumberArray, isBetween, isLengthLessThan, isRequired, validateRequest } from './validations.js'
+'use strict'
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const cors = require('cors');
+const KJUR = require('jsrsasign');
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(express.json(), cors())
-app.options('*', cors())
-
-// Validations should match Zoom Cobrowse SDK's documentation:
-// https://developers.zoom.us/docs/cobrowse-sdk/auth/#payload
-const validator = {
-  role: [isRequired, inNumberArray([1, 2])],
-  userId: isLengthLessThan(35),
-  userName: isLengthLessThan(35),
-  expirationSeconds: isBetween(1800, 172800)
-}
-
-const coerceRequestBody = (body) => ({
-  ...body,
-  ...['role', 'expirationSeconds'].reduce(
-    (acc, cur) => ({ ...acc, [cur]: typeof body[cur] === 'string' ? parseInt(body[cur]) : body[cur] }),
-    {}
-  )
-})
+app.use(bodyParser.json(), cors());
+app.options('*', cors());
 
 app.post('/', (req, res) => {
-  const requestBody = coerceRequestBody(req.body)
-  const validationErrors = validateRequest(requestBody, validator)
+  const iat = Math.round(new Date().getTime() / 1000) - 30;
+  const exp = iat + 60 * 60 * 2;
 
-  if (validationErrors.length > 0) {
-    return res.status(400).json({ errors: validationErrors })
-  }
+  const user_id = req.body.userId ?? Math.random().toString(36).substring(2)
+  const user_name = req.body.userName ?? user_id
 
-  const { role, expirationSeconds, userId, userName } = requestBody
+  const oHeader = { alg: 'HS256', typ: 'JWT' };
 
-  const iat = Math.floor(Date.now() / 1000)
-  const user_id = userId ?? Math.random().toString(36).substring(2)
-  const user_name = userName ?? user_id
-  const exp = expirationSeconds ? iat + expirationSeconds : iat + 60 * 60 * 2
-  const oHeader = { alg: 'HS256', typ: 'JWT' }
 
   const oPayload = {
     app_key: process.env.ZOOM_SDK_KEY,
-    role_type: role,
+    role_type: req.body.role,
     user_id,
     user_name,
     iat,
@@ -59,6 +37,6 @@ app.post('/', (req, res) => {
   return res.json({ token: sdkJWT })
 });
 
-//app.listen(port, () => console.log(`Zoom Cobrowse SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
+app.listen(port, () => console.log(`Zoom Cobrowse SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
 
-export default app;
+//module.exports = app
