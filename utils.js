bf4fabb91c8f549e1b4b9702946626b3f338261c
ajitha-zoom <123167421+ajitha-zoom@users.noreply.github.com>
2025-03-14 18:31:25 -0400

    Delete utils.js

--- utils.js
+++
@@ -1,8 +0,0 @@
-export const isDefined = (str) => !!str
-
-export const isStringArray = (value) => Array.isArray(value) && value.every((x) => typeof x === 'string')
-
-export const isValidationError = (value) =>
-  typeof value?.property !== 'undefined' && typeof value?.reason !== 'undefined'
-
-export const replaceWhitespace = (str) => str.replace(/\s+/g, '')
